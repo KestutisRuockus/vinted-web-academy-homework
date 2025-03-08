942c74d38118ad9bb5dde39cba2cc8a8623732b2
KestutisRuockus <kestutisruockus@gmail.com>
2025-03-09 00:11:13 +0200

    Update UI: - Show 'Favourite' button only on hover; - Add ArrowToScrollUp component. Enable scroll to top functionality;

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import { ActivePage } from "../types/types";
+
+const ArrowToScrollUp = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="50"
+      height="50"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="black"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      style={{
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        cursor: "pointer",
+        background: "white",
+        borderRadius: "50%",
+        padding: "10px",
+        boxShadow: "0px 2px 5px rgba(0,0,0,0.3)",
+      }}
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    >
+      <path d="M12 19V5" />
+      <path d="M5 12l7-7 7 7" />
+    </svg>
+  );
+};
 
 const AppLayout = ({
   children,
@@ -13,6 +43,21 @@ const AppLayout = ({
   resetQuery: () => void;
   activePage: ActivePage;
 }) => {
+  const [isArrowSvgVisible, setIsArrowSvgVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setIsArrowSvgVisible(true);
+      } else {
+        setIsArrowSvgVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       <Navbar
@@ -21,7 +66,10 @@ const AppLayout = ({
         resetQuery={resetQuery}
       />
       <h1 className="title">Assignment</h1>
-      <main>{children}</main>
+      <main style={{ position: "relative" }}>
+        {children}
+        {isArrowSvgVisible && <ArrowToScrollUp />}
+      </main>
     </>
   );
 };
